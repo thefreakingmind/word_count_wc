3346d5cc17245b6dcec5f3a9794a99a9e7f761fb
thefreakingmind <salmansiddiqui100@gmail.com>
2023-12-02 19:20:14 +0530

    Fixed the Byte issue

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 import sys
+import os 
+from pathlib import Path
 
-print(dir(sys))
+
+class ReadArguments(object):
+    def __init__(self, command):
+        self.command = command 
+
+    def process_command(self):
+        return self.command
 
 class FileReader(object):
     def __init__(self, file_name, instruction):
@@ -15,8 +23,13 @@ class FileReader(object):
 
 
 class CalculationProcessor(object):
-    def __init__(self, data):
+    def __init__(self, data, file_name):
         self.data = data
+        self.file_name = file_name 
+
+    def calculate_file_size(self):
+        fileData = os.stat(self.file_name)
+        return fileData.st_size
 
     def read_lines(self):
         word = 0
@@ -33,7 +46,10 @@ class CalculationProcessor(object):
         print("Length of Character is ",  character)
 
 
-file_reader = FileReader("input_text.txt", "r")
-data = file_reader.read_file()
-calculation = CalculationProcessor(data)
-calculation.read_lines()
+if __name__ == '__main__':
+    file_reader = FileReader("input_text.txt", "r")
+    data = file_reader.read_file()
+    calculation = CalculationProcessor(data, "input_text.txt")
+    calculation.read_lines()
+    x = calculation.calculate_file_size()
+    print(x)
